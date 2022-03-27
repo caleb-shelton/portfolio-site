bcced1b45aad40fcadb81161ce30395029981ab1
Caleb Shelton <cshelton@unipart.io>
2022-03-27 21:35:12 +0100

    add code snippet

--- nextjs-portfolio-site/pages/index.js
+++ nextjs-portfolio-site/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RoughNotation } from "react-rough-notation";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 
 export default function Home() {
   return (
@@ -7,37 +9,66 @@ export default function Home() {
       <Head>
         <title>Caleb Shelton</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;1,500&display=swap"
-            rel="stylesheet"
-          />
       </Head> 
 
       <div className="main">
             <div className="bio">
                 <h1>Hello! 👋 I'm
-                  <RoughNotation type="highlight" show={true} color="#fff176"> Caleb</RoughNotation>, a developer based in Cambridge.</h1>
+                  <RoughNotation type="highlight" show={true} color="#fff176"> Caleb</RoughNotation>, a Software Engineer <br /> based in Cambridge.</h1>
                 <div className="paragraph">
                     I am a Cambridge based
-                    <RoughNotation type="underline" show={true} color="#000000"> Software Engineer </RoughNotation> 
+                    <RoughNotation type="underline" animationDelay={1000} show={true} color="#000000"> Software Engineer </RoughNotation> 
                     and
-                    <RoughNotation type="underline" show={true} color="#000000"> First Class Graduate </RoughNotation>
+                    <RoughNotation type="underline" animationDelay={2000} show={true} color="#000000"> First Class Graduate </RoughNotation>
                     of Computer Science. <br /> My department awarded me
-                    <RoughNotation type="highlight" show={true} color="#fff176"> 'the best individual performance by a BSc Computing student'.</RoughNotation>
+                    <RoughNotation type="highlight" animationDelay={4000} show={true} color="#9787FF"> 'the best individual performance by a BSc Computing student'.</RoughNotation>
                     <br />
                     I am highly solution focused and excel in problem solving environments. <br />I work
                     full-time as a Software Engineer at Unipart Digital solving complex logistical
                     challenges using Linux, Python, Docker, and cloud technologies.
                 </div>
                 <div className="buttons">
-                    <button>View LinkedIn</button>
-                    <button>View Github</button>
+                    <a href="https://www.linkedin.com/in/caleb-shelton-46a760143/"><button>View LinkedIn</button></a>
+                    <a href="https://github.com/caleb-shelton"><button>View Github</button></a>
+
                 </div>
             </div><div className="picture">
-                <img src="snippet.svg" />
+            <pre className="line-numbers">
+              <code className="language-js">
+                {`print("hello world!")`}
+              </code>
+            </pre>
+                {/* <img src="snippet.svg" /> */}
                 
             </div>
       </div>
+
+      {/* <div className="main">
+            <div className="half">
+                <h1>Projects 🚀</h1>
+                <div className="paragraph">
+                    <h2>Test Project</h2>
+                    <i><h3>WIP</h3></i>
+                    WIP
+                </div>
+            </div><div className="half">
+                <h1>Skills 💻</h1>
+                <div className="paragraph">
+                    <ul>
+                        <li>Python</li>
+                        <li>Linux</li>
+                        <li>React.js/Next.js</li>
+                        <li>Git</li>
+                        <li>Django/Flask/Odoo</li>
+                        <li>Cloud development</li>
+                        <li>HTML/CSS/UI Design</li>
+                        <li>Adobe CC Suite</li>
+                        <li>Electronics/Systems Engineering</li>
+                        <li>Strong communication and interpersonal skills</li>
+                    </ul>
+                </div>
+            </div>
+        </div> */}
 
       <style jsx global>{`
 
@@ -94,6 +125,10 @@ export default function Home() {
         
         .buttons {
             margin-top: 20px;
+        }
+
+        button {
+            margin-right: 5px;
         }
         
         h1 {
